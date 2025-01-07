9bd05dfd352b7fbd5a5b9578f807681f622333bd
Bo <249181764@qq.com>
2025-01-07 09:18:31 +0800

    update docs

--- docs/assets/alert-img-B2PhIkRN.js
+++
@@ -1,1 +0,0 @@
-const o="/ai-video-docs/imgs/components/bg-video.png",s="/ai-video-docs/imgs/components/alert-img.png";export{o as _,s as a};
---
+++ docs/assets/wordcloud-Dvt3pESu.js
@@ -0,0 +1,1 @@
+const o="/ai-video-docs/imgs/components/bg-video.png",s="/ai-video-docs/imgs/components/alert-img.png",i="/ai-video-docs/imgs/components/wordcloud.png";export{o as _,s as a,i as b};
